caa16a5a62b100c202230be1dc456478614c3949
MickyWillys <fozeum@yahoo.fr>
2014-02-25 11:42:13 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DabbaLanding::Application.config.secret_key_base = '1c9379aeacee039a41f578cc3ed55ec2f6b3214168db8a9ca0f1b9e2d4e044abfd05eae18aa0fd200ef7ba122dcd8861c17b4a99954de54114a56bfdb11e111b'
